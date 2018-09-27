81495c4fd0317b50d8c31d77e377391858cceadc
herminio autonoma <herminiopaucar@gmail.com>
2018-09-26 22:18:10 -0500

    new version with google maps

--- basic_lab05/views/persona/registrado.php
+++ basic_lab05/views/persona/registrado.php
@@ -12,5 +12,35 @@ $this->title = 'Registro de Ubicación';
     <h2>Latitud: <?=$ubicacion->latitud ?></h2>
     <h2>Longitud: <?=$ubicacion->longitud ?></h2>
     
-        
+
+</div>
+
+<div>
+
+<?php 
+
+echo yii2mod\google\maps\markers\GoogleMaps::widget([
+    'userLocations' => [
+        [
+            'location' => [
+                'lat' => $ubicacion->latitud,
+                'lng' => $ubicacion->longitud,
+            ],
+            'htmlContent' => '<h1>Universidad Autonoma</h1>',
+        ],
+        [
+            'location' => [
+                'city' => 'New York',
+                'country' => 'United States',
+            ],
+            'htmlContent' => '<h1>New York</h1>',
+        ],
+    ],
+]);
+
+
+
+
+?>
+
 </div>
